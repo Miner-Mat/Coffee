1c92ba063af580c00c0de1816ffc0d6b94d18ece
User <minermat007@gmail.com>
2023-11-22 18:58:36 +0700

    Commit

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import sys
 
+from PyQt5 import uic
 from PyQt5.QtSql import QSqlDatabase, QSqlTableModel
-from PyQt5.QtWidgets import QWidget, QTableView, QApplication
+from PyQt5.QtWidgets import QWidget, QMainWindow, QTableView, QApplication
 
 
-class MainWindow(QWidget):
+class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
+        uic.loadUi("main.ui", self)
         self.initUI()
 
     def initUI(self):
@@ -17,20 +19,11 @@ class MainWindow(QWidget):
         # И откроем подключение
         db.open()
 
-        # QTableView - виджет для отображения данных из базы
-        view = QTableView(self)
-        # Создадим объект QSqlTableModel,
-        # зададим таблицу, с которой он будет работать,
-        #  и выберем все данные
         model = QSqlTableModel(self, db)
         model.setTable('cofe')
         model.select()
 
-        # Для отображения данных на виджете
-        # свяжем его и нашу модель данных
-        view.setModel(model)
-        view.move(0, 10)
-        view.resize(617, 315)
+        self.tableView.setModel(model)
 
 
 if __name__ == '__main__':
